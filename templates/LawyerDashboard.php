59a9cfb835176a11e417ad64a88bd7addb6a8a85
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2024-12-19 21:35:45 +0100

    Added logout logic in PHP

--- templates/LawyerDashboard.php
+++ templates/LawyerDashboard.php
@@ -1,6 +1,13 @@
 <?php
 include('../config/db.php');
+
 session_start();
+
+if (!isset($_SESSION['user_id']) || !isset($_SESSION['role']) || $_SESSION['role'] !== 'Lawyer') {
+    header("Location: login.php");
+    exit;
+}
+
 $lawyer_id = $_SESSION['user_id'];
 
 $sql = "SELECT User.Name, Lawyer.PhotoURL, Lawyer.Specialization, Lawyer.ExpYears, Lawyer.Bio
@@ -73,7 +80,7 @@ if ($lawyer_result->num_rows > 0) {
             </a>
          </li>
          <li>
-            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+            <a href="logout.php" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
                   <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"/>
                </svg>
